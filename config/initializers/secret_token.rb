f38c2e029f0aa75cb68b3f9c90aff45303076921
Matthew Kandler <matthew.kandler@gmail.com>
2013-06-18 16:28:43 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mattkandler::Application.config.secret_token = '0ef2c62b380564bf2a6971a95fb45775874e18328dfbf6785993cf2cefd153814bc286476a93f6a231ed0eaf7f457db0a478aa466be4f6d044341319abc9d8a7'
